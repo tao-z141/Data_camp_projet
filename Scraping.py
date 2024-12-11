b6d4c0908b574eb4a9251ec1a36cac4122a3dce1
judekabeya <judek9392@gmail.com>
2024-12-11 14:49:15 +0100

    update

--- Scraping.py
+++ Scraping.py
@@ -2,6 +2,8 @@ import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 from bs4 import BeautifulSoup
 
